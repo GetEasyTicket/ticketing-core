a33e72d2def366bd508ea33a9f8f21ffcaf89b1a
jayanta layak <jayanta.bitmesra@gmail.com>
2022-08-05 16:48:02 -0700

    First commit

---
+++ src/main/java/com/learning/ticketingcore/TicketingCoreApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ticketingcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingCoreApplication.class, args);
+	}
+
+}
